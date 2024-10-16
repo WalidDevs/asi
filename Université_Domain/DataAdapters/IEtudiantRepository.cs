0facf19b4c4258ba0b996cef5ba883c3dcb9c614
WalidDevs <walidelalami05@gmail.com>
2024-10-16 18:00:23 +0200

    Adding the exceptions so we fixed the error that occure

---
+++ Université_Domain/DataAdapters/IEtudiantRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Université_Domain.Entities;
+ 
+namespace Université_Domain.DataAdapters;
+ 
+public interface IEtudiantRepository 
+{
+    Task<Etudiant> CreateAsync(Etudiant entity);
+    Task UpdateAsync(Etudiant entity);
+    Task DeleteAsync(long id);
+    Task DeleteAsync(Etudiant entity);
+    Task<Etudiant?> FindAsync(long id);
+    Task<Etudiant?> FindAsync(params object[] keyValues);
+    Task<List<Etudiant>> FindByConditionAsync(Expression<Func<Etudiant, bool>> condition);
+    Task<List<Etudiant>> FindAllAsync();
+    Task SaveChangesAsync();
+}
