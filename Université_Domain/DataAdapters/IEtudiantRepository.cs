954b1707eb756c39d7599a09e192261a6fd6844e
WalidDevs <walidelalami05@gmail.com>
2024-11-06 14:30:11 +0100

    adding generic repo and use it with inheritance with etudiant repo

--- Université_Domain/DataAdapters/IEtudiantRepository.cs
+++ Université_Domain/DataAdapters/IEtudiantRepository.cs
@@ -3,15 +3,7 @@ using Université_Domain.Entities;
  
 namespace Université_Domain.DataAdapters;
  
-public interface IEtudiantRepository 
+public interface IEtudiantRepository :IRepository<Etudiant>
 {
-    Task<Etudiant> CreateAsync(Etudiant entity);
-    Task UpdateAsync(Etudiant entity);
-    Task DeleteAsync(long id);
-    Task DeleteAsync(Etudiant entity);
-    Task<Etudiant?> FindAsync(long id);
-    Task<Etudiant?> FindAsync(params object[] keyValues);
-    Task<List<Etudiant>> FindByConditionAsync(Expression<Func<Etudiant, bool>> condition);
-    Task<List<Etudiant>> FindAllAsync();
-    Task SaveChangesAsync();
+    
 }
