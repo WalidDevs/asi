825dd30dbd6a8df3ceca50a9701bb79ab34e78a9
WalidDevs <walidelalami05@gmail.com>
2024-11-06 17:30:28 +0100

    adding the OneToMany relation between the parcours and etudiant class and test passed succesfully

--- Université_Domain/DataAdapters/IParcoursRepository.cs
+++ Université_Domain/DataAdapters/IParcoursRepository.cs
@@ -4,5 +4,8 @@ namespace Université_Domain.DataAdapters;
 
 public interface IParcoursRepository : IRepository<Parcours>
 {
-    
+    Task<Parcours> AddEtudiantAsync(Parcours parcours, Etudiant etudiant);
+    Task<Parcours> AddEtudiantAsync(long idParcours, long idEtudiant);
+    Task<Parcours> AddEtudiantAsync(Parcours ? parcours, List<Etudiant> etudiants);
+    Task<Parcours> AddEtudiantAsync(long idParcours, long[] idEtudiants);
 }
